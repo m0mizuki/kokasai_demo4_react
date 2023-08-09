b42aca6a1f302cba276818a572f2411132a71c27
m0mizuki <mutou.m.2020@gmail.com>
2023-08-09 10:11:12 +0900

    404ページとindexにスクリプト追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/Home';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/home"><Home /></Route>
+        </Switch>
+      </BrowserRouter>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +27,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <p><a href="/home">ホームへ</a></p>
+
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+        <h1>こんにちは</h1>
+    </div>
+  );
+}
+
+export default Home;
